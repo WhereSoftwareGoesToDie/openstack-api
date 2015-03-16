c87e319f5131a594d825898d23d27aade9656a38
Timo von Holtz <tvh@anchor.com.au>
2015-03-16 16:25:17 +1100

    Remove v2 endpoint and implement v3

--- lib/OpenStack/Keystone.hs
+++ lib/OpenStack/Keystone.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE FlexibleInstances     #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE OverloadedStrings     #-}
+{-# LANGUAGE RecordWildCards       #-}
 {-# LANGUAGE TemplateHaskell       #-}
 {-# LANGUAGE TypeFamilies          #-}
 {-# LANGUAGE TypeOperators         #-}
@@ -10,13 +11,16 @@ module OpenStack.Keystone where
 import           Control.Applicative
 import           Control.Lens.Operators     ((^.))
 import           Control.Lens.TH
+import           Control.Monad.Error
 import           Control.Monad.Trans.Either
+import           Control.Monad.Trans.Except
 import           Data.Aeson
-import qualified Data.HashMap.Strict        as H
+import           Data.Coerce
 import           Data.Monoid
 import           Data.Proxy
 import           Data.Text                  (Text)
-import           Data.Time.Clock
+import           Network.HTTP.Client        (Response (..))
+import           Network.HTTP.Types         (Status (..))
 import           Servant.API
 import           Servant.Client
 
@@ -34,121 +38,41 @@ instance ToJSON TokenRequest where
       where
         auth :: Value
         auth = object
-            [ "tenantName" .= (x ^. requestTenantName)
-            , "passwordCredentials" .= passwordCredentials ]
+            ([ "identity" .= identity ] <> [ "scope" .= scope' | Just scope' <- pure scope ])
+        domain :: Value
+        domain = object [ "name" .= ("Default" :: String) ]
+        identity :: Value
+        identity = object
+            [ "methods" .= [ "password" :: String ]
+            , "password" .= object [ "user" .= passwordCredentials ] ]
         passwordCredentials :: Value
         passwordCredentials = object
-            [ "username" .= (x ^. requestUsername)
-            , "password" .= (x ^. requestPassword) ]
-
-data Tenant = Tenant
-    { _tenantId          :: TenantId
-    , _tenantName        :: Text
-    , _tenantEnabled     :: Bool
-    , _tenantDescription :: Text
-    } deriving (Eq, Show)
-makeLenses ''Tenant
-
-instance FromJSON Tenant where
-    parseJSON (Object o) = Tenant
-        <$> o .: "id"
-        <*> o .: "name"
-        <*> o .: "enabled"
-        <*> o .: "description"
-    parseJSON _ = mempty
-
-data Token = Token
-    { _tokenIssuedAt :: UTCTime
-    , _tokenExpires  :: UTCTime
-    , _tokenId       :: TokenId
-    , _tokenTenant   :: Maybe Tenant
-    } deriving (Eq, Show)
-makeLenses ''Token
-
-instance FromJSON Token where
-    parseJSON (Object o) = Token
-        <$> (unISO8601Time <$> o .: "issued_at")
-        <*> (unISO8601Time <$> o .: "expires")
-        <*> o .: "id"
-        <*> o .:? "tenant"
-    parseJSON _ = mempty
-
-data Endpoint = Endpoint
-    { _endpointAdminUrl    :: Text
-    , _endpointInternalUrl :: Text
-    , _endpointPublicUrl   :: Text
-    , _endpointRegion      :: Text
-    , _endpointId          :: Text
-    } deriving (Eq, Show)
-
-instance FromJSON Endpoint where
-    parseJSON (Object o) = Endpoint
-        <$> o .: "adminURL"
-        <*> o .: "internalURL"
-        <*> o .: "publicURL"
-        <*> o .: "region"
-        <*> o .: "id"
-    parseJSON _ = mempty
-
-data Service = Service
-    { _serviceEndpoints :: [Endpoint]
-    , _serviceType      :: Text
-    , _serviceName      :: Text
-    } deriving (Eq, Show)
-makeLenses ''Service
-
-instance FromJSON Service where
-    parseJSON (Object o) = Service
-        <$> o .: "endpoints"
-        <*> o .: "type"
-        <*> o .: "name"
-    parseJSON _ = mempty
-
-data Role = Role
-    { _roleName :: Text
-    } deriving (Eq, Show)
-
-instance FromJSON Role where
-    parseJSON (Object o) = Role
-        <$> o .: "name"
-    parseJSON _ = mempty
-
-data User = User
-    { _userUserName :: Text
-    , _userId       :: UserId
-    , _userRoles    :: [Role]
-    , _userName     :: Text
-    } deriving (Eq, Show)
-
-instance FromJSON User where
-    parseJSON (Object o) = User
-        <$> o .: "username"
-        <*> o .: "id"
-        <*> o .: "roles"
-        <*> o .: "name"
-    parseJSON _ = mempty
-
-data TokenResponse = TokenResponse
-    { _responseToken          :: Token
-    , _responseServiceCatalog :: [Service]
-    , _responseUser           :: User
-    } deriving (Eq, Show)
-
-instance FromJSON TokenResponse where
-    parseJSON (Object o) =
-        case H.lookup "access" o of
-            (Just (Object o')) -> TokenResponse
-                <$> o' .: "token"
-                <*> o' .: "serviceCatalog"
-                <*> o' .: "user"
-            _ -> mempty
-    parseJSON _ = mempty
+            [ "name" .= (x ^. requestUsername)
+            , "password" .= (x ^. requestPassword)
+            , "domain" .= domain ]
+        scope :: Maybe Value
+        scope = do
+            project <- x ^. requestTenantName
+            pure $ object [ "project" .= object [ "domain" .= domain, "name" .= project ] ]
 
 type KeystoneApi =
-         "v2.0" :> "tokens" :> ReqBody '[JSON] TokenRequest :> Post '[JSON] TokenResponse
+         "v3" :> "auth" :> "tokens" :> ReqBody '[JSON] TokenRequest :> Raw
 
 keystoneApi :: Proxy KeystoneApi
 keystoneApi = Proxy
 
-requestToken :: TokenRequest -> BaseUrl -> EitherT ServantError IO TokenResponse
-requestToken = client keystoneApi
+data KeystoneMethods = KeystoneMethods
+    { requestToken :: TokenRequest -> ExceptT ServantError IO TokenId
+    }
+
+keyStoneMethods :: String -> Either String KeystoneMethods
+keyStoneMethods url = do
+    baseUrl <- parseBaseUrl url
+    let (requestToken') = client keystoneApi
+        requestToken req = coerce $ do
+            (status,body,ct,res) <- requestToken' req "POST" baseUrl
+            unless (status == 201) $ throwError $ FailureResponse (Status status "") ct body
+            case lookup "X-Subject-Token" $ responseHeaders res of
+                Nothing -> throwError $ DecodeFailure "X-Subject-Token not present" ct body
+                Just x -> return $ TokenId x
+    return KeystoneMethods{..}
